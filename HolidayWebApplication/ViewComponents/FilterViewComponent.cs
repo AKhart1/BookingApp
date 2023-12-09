31d75013d6a7a10aac4d8c19c3ad5369f01e4917
Alex Khartaniuk <S200823@sggw.edu.pl>
2023-12-09 18:27:23 +0100

    Added Image model, component to Filter, View user manager panel

---
+++ HolidayWebApplication/ViewComponents/FilterViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationLayer.ViewComponents
+{
+    public class FilterViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
